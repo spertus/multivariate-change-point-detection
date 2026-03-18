5a80376396dbab50337f848891807f7bbc8bd59b
spertus <jakespertus@gmail.com>
2026-03-18 07:18:00 -0400

    added tests for combiners

--- tests/testthat/test-combiners.R
+++ tests/testthat/test-combiners.R
@@ -38,3 +38,20 @@ test_that("universal portfolio combiner is positive", {
   z <- combine_streams(up, cbind(s1, s2))
   expect_true(all(z > 0))
 })
+
+test_that("product beats universal portfolio beats average", {
+  s1 <- runif(20, 0.9, 1.3)
+  s2 <- runif(20, 0.9, 1.3)
+
+  p <- ProductCombiner()
+  a <- AverageCombiner()
+  up <- UniversalPortfolioCombiner(resolution = 4)
+  z_p <- combine_streams(p, cbind(s1, s2))
+  z_a <- combine_streams(a, cbind(s1, s2))
+  z_up <- combine_streams(up, cbind(s1, s2))
+  expect_true(all(z_p > 0))
+  expect_true(all(z_up > 0))
+  expect_true(all(z_a > 0))
+  expect_true(z_p[length(z_p)] > z_up[length(z_up)])
+  expect_true(z_up[length(z_up)] > z_a[length(z_a)])
+})
